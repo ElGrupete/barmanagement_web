df3f838ef80c39ae6f149895cd0a8e5f19161b9b
Eric Farley <ericfarley96@gmail.com>
2020-09-13 00:15:53 -0300

    Categories' components

--- src/app/core/modules/management/menus/menus.module.ts
+++ src/app/core/modules/management/menus/menus.module.ts
@@ -4,10 +4,18 @@ import { CommonModule } from '@angular/common';
 
 import { MenusRoutingModule } from './menus-routing.module';
 import { MenusComponent } from './menus.component';
+import { NewCategoryComponent } from './category/components/new-category/new-category.component';
+import { CategoryListComponent } from './category/components/category-list/category-list.component';
+import { CategoryDetailComponent } from './category/components/category-detail/category-detail.component';
 
 
 @NgModule({
-  declarations: [MenusComponent],
+  declarations: [
+    MenusComponent,
+    NewCategoryComponent,
+    CategoryListComponent,
+    CategoryDetailComponent, 
+  ],
   imports: [
     CommonModule,
     MenusRoutingModule,
